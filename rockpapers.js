55d18e9f67632a25f0385b4d81eda0e0fcfec5e6
Kevin Karikurubu <31185705+kevinkarikurubu@users.noreply.github.com>
2022-07-17 13:25:42 +0300

    third commit changes

--- rockpapers.js
+++ rockpapers.js
@@ -1,38 +1,68 @@
-let gameresult;
-      cont player_options = ["rock", "paper", "scissors"];
-      let winner;
-      let i = 0;
+const playGame = ["rock", "paper", "scissors"];
+let playerSelection = prompt(
+  "feel free to choose one of these: rock, paper or scissors"
+).toLocaleLowerCase();
+let computerSelection = playGame[(Math.random() * playGame.length) | 0];
+let playerWinCount = 0;
+let computerWinCount = 0;
+let roundResult = 0;
+console.log("You chose " + playerSelection);
+console.log("The computer chose " + computerSelection);
+ console.log("Play again please");
 
-      function playGame(playerSelection, computerSelection) {
-        const computerSelection =
-          player_options[Math.floor(Math.random() * player_options.length)];
-        let playerSelection = window.prompt(
-          "Among the three Who will win? Rock,Papers and Scissors"
-        );
-        gameresult = playerSelection.localeCompare(computerSelection);
-        console.log(gameresult);
-        console.log(computerSelection);
-        i = i + 1;
+function computerPlay() {
+  let computerChoose = playGame[(Math.random() * playGame.length) | 0];
+  console.log(computerChoose);
+  
+}
 
-        if (gameresult == 0) {
-          winner = "You have Won";
-          document.write("Game No.");
-          document.write(i);
-          window.alert("You have Won");
-          document.write("   You won, congs pick your cash from the counter <br>");
-        } else {
-          winner = "Computer has Won";
-          document.write("Game No.");
-          document.write(i);
-          document.write("  You lost,<br>");
-          window.alert("You have lost, try again");
-        }
-        console.log(winner);
-        return winner;
-      }
-      playGame();
-      const playagain = window.confirm("Do you want to play again?");
-      console.log(playagain);
-      while (playagain == true) {
-        playGame();
-      }
+function playRound(playerSelection, computerSelection) {
+  
+  if (playerSelection === "rock" && computerSelection === "rock") {
+    alert("It's a tie");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    alert("You lose! Paper beats rock");
+    computerWinCount++;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    alert("You win! Rock beats scissors");
+    playerWinCount++;
+  }
+
+  else if (playerSelection === "paper" && computerSelection === "paper") {
+  
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    
+    computerWinCount++;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    
+    playerWinCount++;
+  }
+
+  else if (playerSelection === "scissors" && computerSelection === "scissors") {
+    
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    
+    computerWinCount++;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    
+    playerWinCount++;
+  }
+  return roundResult;
+}
+console.log("You have won " + playerWinCount);
+console.log("The computer has lost " + computerWinCount);
+
+function game() {
+  for (let i = 1; i < 5; i++) {
+    playerSelection = prompt(
+      "Please write rock, paper or scissors"
+    ).toLocaleLowerCase();
+    computerSelection = playGame[(Math.random() * playGame.length) | 0];
+    playRound(playerSelection, computerSelection);
+    console.log("You chose " + playerSelection);
+    console.log("The computer chose " + computerSelection);
+    console.log("another round");
+    console.log(playerWinCount);
+    console.log(computerWinCount);
+  }
+}
