2ad9ceb1b66a3e1b26bd306a43039526bcfdec7d
Kevin Karikurubu <31185705+kevinkarikurubu@users.noreply.github.com>
2022-07-19 03:14:04 +0300

    new commit changes

--- rockpapers.js
+++ rockpapers.js
@@ -1,23 +1,34 @@
-const playGame = ["rock", "paper", "scissors"];
+"use strict";
+
+const myList = ["rock", "paper", "scissors"];
 let playerSelection = prompt(
-  "feel free to choose one of these: rock, paper or scissors"
+  "Please write rock, paper or scissors"
 ).toLocaleLowerCase();
-let computerSelection = playGame[(Math.random() * playGame.length) | 0];
+let computerSelection = myList[(Math.random() * myList.length) | 0];
 let playerWinCount = 0;
 let computerWinCount = 0;
 let roundResult = 0;
+
 console.log("You chose " + playerSelection);
 console.log("The computer chose " + computerSelection);
- console.log("Play again please");
+console.log("New round");
+playRound(playerSelection, computerSelection);
+game();
 
 function computerPlay() {
-  let computerChoose = playGame[(Math.random() * playGame.length) | 0];
-  console.log(computerChoose);
-  
+  let computerTurn = myList[(Math.random() * myList.length) | 0];
+  console.log(computerTurn);
+  computerSelection = computerPlay();
+  playerSelection = prompt(
+    "Please write rock, paper or scissors"
+  ).toLowerCase();
+  let roundResult;
+  return computerSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
-  
+  //playerSelection rock
+
   if (playerSelection === "rock" && computerSelection === "rock") {
     alert("It's a tie");
   } else if (playerSelection === "rock" && computerSelection === "paper") {
@@ -27,42 +38,52 @@ function playRound(playerSelection, computerSelection) {
     alert("You win! Rock beats scissors");
     playerWinCount++;
   }
-
+  //playerSelection paper
   else if (playerSelection === "paper" && computerSelection === "paper") {
-  
+    alert("It's a tie");
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    
+    alert("You lose! scissors beats paper");
     computerWinCount++;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    
+    alert("You win! Paper beats rock");
     playerWinCount++;
   }
-
+  //playerSelection scissors
   else if (playerSelection === "scissors" && computerSelection === "scissors") {
-    
+    alert("It's a tie");
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    
+    alert("You lose! Rock beats scissors");
     computerWinCount++;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    
+    alert("You win! Scissors beats paper");
     playerWinCount++;
   }
   return roundResult;
 }
-console.log("You have won " + playerWinCount);
-console.log("The computer has lost " + computerWinCount);
+console.log("You have won " + playerWinCount + " rounds");
+console.log("The computer has won " + computerWinCount + " rounds");
 
 function game() {
   for (let i = 1; i < 5; i++) {
     playerSelection = prompt(
       "Please write rock, paper or scissors"
-    ).toLocaleLowerCase();
-    computerSelection = playGame[(Math.random() * playGame.length) | 0];
+    ).toLowerCase();
+    computerSelection = myList[(Math.random() * myList.length) | 0];
     playRound(playerSelection, computerSelection);
     console.log("You chose " + playerSelection);
     console.log("The computer chose " + computerSelection);
-    console.log("another round");
+    console.log("Play again");
     console.log(playerWinCount);
     console.log(computerWinCount);
+  }
+  playRound();
+  if (playerWinCount > computerWinCount) {
+    alert(
+      `You won ${playerWinCount} rounds and the computer won ${computerWinCount} You won`
+    );
+  } else {
+    alert(
+      `The computer won ${computerWinCount} rounds and you won ${playerWinCount} rounds. You lost`
+    );
   }
 }
