a2d59f04e9aca296a37631a0ce82a2e3ac45add9
Kevin Karikurubu <31185705+kevinkarikurubu@users.noreply.github.com>
2022-07-26 15:59:21 +0300

    NEW COMMIT

--- rockpapers.js
+++ rockpapers.js
@@ -1,89 +1,67 @@
-"use strict";
-
-const myList = ["rock", "paper", "scissors"];
-let playerSelection = prompt(
-  "Please write rock, paper or scissors"
-).toLocaleLowerCase();
-let computerSelection = myList[(Math.random() * myList.length) | 0];
-let playerWinCount = 0;
-let computerWinCount = 0;
-let roundResult = 0;
-
-console.log("You chose " + playerSelection);
-console.log("The computer chose " + computerSelection);
-console.log("New round");
-playRound(playerSelection, computerSelection);
-game();
-
+alert("Welcome to a Game where you play five rounds!");
+let computerWin = 0;
+let playerWin = 0;
+const computerSelection = ["rock", "paper", "scissors"];
+for (let i = 1; i <= 5; i++) {
+  let answer = prompt(`Fill in Rock, Paper or Scissors please: `, `${i}`);
+  if (answer === null || answer === undefined) {
+    break;
+  }
+  let computerSelection = computerPlay();
+  while (
+    answer.length >= 5 &&
+    answer.length <= 10 &&
+    testinput(answer) === true
+  ) {
+    if (answer.toLowerCase() === computerSelection) {
+      console.log(
+        `The computer says ${computerSelection}, and you say ${answer}, you win, it's a draw!`
+      );
+      playerWin++;
+      break;
+    } else if (answer.toLowerCase() < computerSelection) {
+      console.log(
+        `The computer says ${computerSelection}, and you say ${answer}, the computer wins!`
+      );
+      computerWin++;
+      break;
+    } else if (answer.toLowerCase() > computerSelection) {
+      console.log(
+        `The computer says ${computerSelection}, and you say ${answer}, you win!`
+      );
+      playerWin++;
+      break;
+    }
+  }
+  if (answer.length < 5) {
+    computerWin++;
+    alert(
+      `The computer says ${computerSelection}, and you say ${answer}, your input is not right, you lose this round!`
+    );
+  } else if (answer.length > 10) {
+    computerWin++;
+    alert(
+      `The computer says ${computerSelection}, and you say ${answer}, your input is not right, you lose this round!`
+    );
+  }
+}
+function testinput(answer) {
+  switch (answer) {
+    case computerSelection[0]:
+      return true;
+    case computerSelection[1]:
+      return true;
+    case computerSelection[2]:
+      return true;
+  }
+  return false;
+}
 function computerPlay() {
-  let computerTurn = myList[(Math.random() * myList.length) | 0];
-  console.log(computerTurn);
-  computerSelection = computerPlay();
-  playerSelection = prompt(
-    "Please write rock, paper or scissors"
-  ).toLowerCase();
-  let roundResult;
-  return computerSelection;
+  return computerSelection[Math.floor(Math.random() * 3)];
 }
-
-function playRound(playerSelection, computerSelection) {
-  //playerSelection rock
-
-  if (playerSelection === "rock" && computerSelection === "rock") {
-    alert("It's a tie");
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    alert("You lose! Paper beats rock");
-    computerWinCount++;
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    alert("You win! Rock beats scissors");
-    playerWinCount++;
-  }
-  //playerSelection paper
-  else if (playerSelection === "paper" && computerSelection === "paper") {
-    alert("It's a tie");
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    alert("You lose! scissors beats paper");
-    computerWinCount++;
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    alert("You win! Paper beats rock");
-    playerWinCount++;
-  }
-  //playerSelection scissors
-  else if (playerSelection === "scissors" && computerSelection === "scissors") {
-    alert("It's a tie");
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    alert("You lose! Rock beats scissors");
-    computerWinCount++;
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    alert("You win! Scissors beats paper");
-    playerWinCount++;
-  }
-  return roundResult;
+if (playerWin > computerWin) {
+  console.log(`In total you win with ${playerWin} to ${computerWin}`);
+} else {
+  console.log(`You lose with ${computerWin} to ${playerWin}`);
 }
-console.log("You have won " + playerWinCount + " rounds");
-console.log("The computer has won " + computerWinCount + " rounds");
-
-function game() {
-  for (let i = 1; i < 5; i++) {
-    playerSelection = prompt(
-      "Please write rock, paper or scissors"
-    ).toLowerCase();
-    computerSelection = myList[(Math.random() * myList.length) | 0];
-    playRound(playerSelection, computerSelection);
-    console.log("You chose " + playerSelection);
-    console.log("The computer chose " + computerSelection);
-    console.log("Play again");
-    console.log(playerWinCount);
-    console.log(computerWinCount);
-  }
-  playRound();
-  if (playerWinCount > computerWinCount) {
-    alert(
-      `You won ${playerWinCount} rounds and the computer won ${computerWinCount} You won`
-    );
-  } else {
-    alert(
-      `The computer won ${computerWinCount} rounds and you won ${playerWinCount} rounds. You lost`
-    );
-  }
-}
+console.log("This is the end of the game!");
